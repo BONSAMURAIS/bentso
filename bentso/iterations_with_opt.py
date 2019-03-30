9c7118e748a8b22bf7ef11300275453b2d73d6ee
Carlos Gaete <cdgaete@gmail.com>
2019-03-30 17:09:46 +0100

    Update iterations_with_opt.py
    directory to store files added

--- bentso/iterations_with_opt.py
+++ bentso/iterations_with_opt.py
@@ -1,10 +1,15 @@
 import pandas as pd
 from .client import CachingDataClient
+from .filesystem import create_dir
 from .storage_abstract_model import *
+import os
 
 c = CachingDataClient()
 
-def InterateCountriesProduction(year):
+def InterateCountriesProduction_with_phs_consumption_subtract(year):
+    cwd = os.getcwd()
+    dest_dir = os.path.join(cwd,'DB_phs_consumption_subtract')
+    create_dir(dest_dir)
     # toogle countries vector; short one just for test
     countries = ['AT','BA','BE','BG','BY','CH','CZ','DE','DK','EE','ES','FI','FR','GB','GB-NIR','GR','HR','HU','IE','IT','LT','LU','LV','ME','MK','MT','NL','NO','PL','PT','RO','RS','RU','RU-KGD','SE','SI','SK','TR','UA'];
     mom = list()
@@ -13,7 +18,7 @@ def InterateCountriesProduction(year):
         try:
             ctr_year = c.get_generation(ctr, year)
         except:
-            print('error after querring ', ctr)
+            print('error after querring due to the country '+ ctr +' is not in the ENTSO-e database)
             continue
         try:
             ctr_year = PHS_consumption_subtraction(ctr,year)
@@ -26,32 +31,16 @@ def InterateCountriesProduction(year):
         a = list(x.index)
         s = pd.Series(ctr)
         pap =  pd.DataFrame(data={'technology': list(x.index), 'country': list(s.repeat(len(x.index))), 'energy_MJ': list(x)})
-        pap.to_csv('gen_opt/'+'gen_'+ctr+'_'+str(year)+'.csv',index=False)
+        pap.to_csv(os.path.join(dest_dir,gen_'+ctr+'_'+str(year)+'.csv'),index=False)
         mom.append(pap)
         
-    f=open("gen_opt/countries_omitted.dat", "w+")
+    f=open(os.path.join(dest_dir,"countries_omitted.dat"), "w+")
     for k, v in country_not_included.items():
          f.write("Omitted due to error in data of generation or prices during optimization %s := %d; \r\n"%(k,v))
     f.close()
     if len(mom) > 1:
         df = pd.concat(mom, ignore_index=True)
-        df.to_csv('gen_opt/'+'gen_'+str(year)+'.csv',index=False)
+        df.to_csv(os.path.join(dest_dir,'gen_'+str(year)+'.csv'),index=False)
     else:
         df = pd.DataFrame()
-    return(df)
-	
-def InterateCountriesTrade(year):
-    # toogle countries vector; short one just for test
-    # countries = ['AT','BA','BE','BG','BY','CH','CZ','DE','DK','EE','ES','FI','FR','GB','GB-NIR','GR','HR','HU','IE','IT','LT','LU','LV','ME','MK','MT','NL','NO','PL','PT','RO','RS','RU','RU-KGD','SE','SI','SK','TR','UA'];
-    countries = ['ES','FR'];
-    mom = list()
-    for fromCtr in countries:
-        for toCtr in countries:
-            if(fromCtr != toCtr):
-                trd_year = c.get_trade(fromCtr, toCtr, year)
-                x = trd_year.sum() * 3600
-                pap =  pd.DataFrame(data={'fromCountry': fromCtr, 'toCountry': toCtr, 'energy_MJ':x}, index=[0])
-                mom.append(pap)
-    
-    df = pd.concat(mom, ignore_index=True)
     return(df)
